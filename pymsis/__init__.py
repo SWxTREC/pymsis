b2da4f5da4cfebc3d3113f991bcf97433da23fa7
Greg Lucas <greg.lucas@lasp.colorado.edu>
2021-08-30 08:40:22 -0600

    MNT: Adding DLL search path for Windows
    Windows needs a way to search for the compiled DLLs relative to
    this package. Once installed, we use the reference to the __init__
    file to locate where the libraries were put and add it to the
    DLL search path or PATH environment variable for this session.

--- pymsis/__init__.py
+++ pymsis/__init__.py
@@ -1,1 +1,25 @@
+import os
 __version__ = "0.2.0"
+
+# If we are on Windows, Python 3.8+ then we need to add a DLL search path
+# The libraries are located relative to this init file.
+if os.name == 'nt':
+    pymsis_dir = None
+    pymsis_dir_libs = None
+    try:
+        # add folder to Windows DLL search paths
+        pymsis_dir = os.path.abspath(os.path.dirname(__file__))
+        pymsis_dir_libs = os.path.join(pymsis_dir, '.libs')
+        try:
+            # This was added in Python 3.8, so we can default to this
+            # once we only support 3.8+
+            os.add_dll_directory(pymsis_dir)
+            # Additionally, we need the .libs directory which has gfortran
+            os.add_dll_directory(pymsis_dir_libs)
+        except Exception:
+            pass
+        os.environ['PATH'] = (f"{pymsis_dir};{pymsis_dir_libs};"
+                              f"{os.environ['PATH']}")
+    except Exception:
+        pass
+    del pymsis_dir, pymsis_dir_libs
