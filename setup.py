9dc1a45f55175d960b794fc9c36df24af5632fa7
Greg Lucas <greg.m.lucas@gmail.com>
2022-08-17 07:29:43 -0600

    BLD: import setuptools before numpy distutils
    Import setuptools before numpy.distutils when installing the package.

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@
 """The setup script."""
 import os
 import sys
-from setuptools import Extension, setup
+import setuptools
+from numpy.distutils.core import Extension, setup
 
 # Some system's don't include the local directory on the
 # base Python path for some reason, so add it in directly
