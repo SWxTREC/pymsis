a78eecbc18e66cd9c41819e1e9030ebceb190551
GitHub Action <action@github.com>
2022-08-18 21:59:47 +0000

    Update documentation

--- _downloads/106f249e4d37ffc37380f194b369e606/plot_altitude_profiles.py
+++ _downloads/106f249e4d37ffc37380f194b369e606/plot_altitude_profiles.py
@@ -20,11 +20,11 @@ alts = np.linspace(0, 1000, 1000)
 f107 = 150
 f107a = 150
 ap = 7
-aps = [[ap]*7]
+aps = [[ap] * 7]
 
-date = np.datetime64('2003-01-01T00:00')
+date = np.datetime64("2003-01-01T00:00")
 output_midnight = msis.run(date, lon, lat, alts, f107, f107a, aps)
-date = np.datetime64('2003-01-01T12:00')
+date = np.datetime64("2003-01-01T12:00")
 output_noon = msis.run(date, lon, lat, alts, f107, f107a, aps)
 
 #  output is now of the shape (1, 1, 1, 1000, 11)
@@ -32,24 +32,36 @@ output_noon = msis.run(date, lon, lat, alts, f107, f107a, aps)
 output_midnight = np.squeeze(output_midnight)
 output_noon = np.squeeze(output_noon)
 
-variables = ['Total mass density', 'N2', 'O2', 'O', 'He',
-             'H', 'Ar', 'N', 'Anomalous O', 'NO', 'Temperature']
+variables = [
+    "Total mass density",
+    "N2",
+    "O2",
+    "O",
+    "He",
+    "H",
+    "Ar",
+    "N",
+    "Anomalous O",
+    "NO",
+    "Temperature",
+]
 
 _, ax = plt.subplots()
 for i, label in enumerate(variables):
-    if label in ('NO', 'Total mass density', 'Temperature'):
+    if label in ("NO", "Total mass density", "Temperature"):
         # There is currently no NO data, also ignore non-number densities
         continue
-    line, = ax.plot(output_midnight[:, i], alts, linestyle='--')
+    (line,) = ax.plot(output_midnight[:, i], alts, linestyle="--")
     ax.plot(output_noon[:, i], alts, c=line.get_color(), label=label)
 
-ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),
-          fancybox=True, shadow=True, ncol=4)
+ax.legend(
+    loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True, ncol=4
+)
 ax.set_title(f"Longitude: {lon}, Latitude: {lat}")
-ax.set_xscale('log')
+ax.set_xscale("log")
 ax.set_xlim(1e8, 1e18)
 ax.set_ylim(0, 1000)
-ax.set_xlabel('Number density (/m$^3$)')
-ax.set_ylabel('Altitude (km)')
+ax.set_xlabel("Number density (/m$^3$)")
+ax.set_ylabel("Altitude (km)")
 
 plt.show()
--- _downloads/71b3dd991db97808869c0e5350de7fe6/plot_annual_variation.py
+++ _downloads/71b3dd991db97808869c0e5350de7fe6/plot_annual_variation.py
@@ -19,13 +19,12 @@ f107 = 150
 f107a = 150
 ap = 7
 # One years worth of data at the 12th hour every day
-dates = np.arange('2003-01', '2004-01',
-                  dtype='datetime64[D]') + np.timedelta64(12, 'h')
+dates = np.arange("2003-01", "2004-01", dtype="datetime64[D]") + np.timedelta64(12, "h")
 ndates = len(dates)
 # (F107, F107a, ap) all need to be specified at the same length as dates
-f107s = [f107]*ndates
-f107as = [f107a]*ndates
-aps = [[ap]*7]*ndates
+f107s = [f107] * ndates
+f107as = [f107a] * ndates
+aps = [[ap] * 7] * ndates
 
 output = msis.run(dates, lon, lat, alt, f107s, f107as, aps)
 #  output is now of the shape (ndates, 1, 1, 1, 11)
@@ -33,21 +32,33 @@ output = msis.run(dates, lon, lat, alt, f107s, f107as, aps)
 output = np.squeeze(output)
 
 # Lets get the percent variation from the annual mean for each variable
-variation = 100*(output/output.mean(axis=0) - 1)
+variation = 100 * (output / output.mean(axis=0) - 1)
 
-variables = ['Total mass density', 'N2', 'O2', 'O', 'He',
-             'H', 'Ar', 'N', 'Anomalous O', 'NO', 'Temperature']
+variables = [
+    "Total mass density",
+    "N2",
+    "O2",
+    "O",
+    "He",
+    "H",
+    "Ar",
+    "N",
+    "Anomalous O",
+    "NO",
+    "Temperature",
+]
 
 _, ax = plt.subplots()
 for i, label in enumerate(variables):
-    if label == 'NO':
+    if label == "NO":
         # There is currently no NO data
         continue
     ax.plot(dates, variation[:, i], label=label)
 
-ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),
-          fancybox=True, shadow=True, ncol=5)
+ax.legend(
+    loc="upper center", bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True, ncol=5
+)
 ax.set_xlabel(f"Longitude: {lon}, Latitude: {lat}, Altitude: {alt} km")
-ax.set_ylabel('Difference from annual mean (%)')
+ax.set_ylabel("Difference from annual mean (%)")
 
 plt.show()
